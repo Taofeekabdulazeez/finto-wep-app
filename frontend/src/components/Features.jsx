d4ee3dbdc01a30443e5fa264d4a466b9e64e78b7
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-09 18:45:51 +0100

    c

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,5 +1,73 @@
+import Button from "../ui/Button";
+
 function Features() {
-  return <div>Features</div>;
+  return (
+    <section className="py-6">
+      <div className="w-[90%] max-w-[100rem] mx-auto">
+        <div className="mb-8">
+          <span className="text-sm text-stone-400">Solutions</span>
+          <h2 className="text-[2rem] font-bold mb-6">
+            Open Your Business Account Online
+          </h2>
+          <p className="text-[1.1rem] text-stone-600 font-medium">
+            Keep your business account and all your finance needs safely
+            organized under one roof. Manage money quickly, easily &
+            efficiently. Whether you’re alone or leading a team.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 gap-8">
+          <div>
+            <span className="text-sm text-stone-400">Payment</span>
+            <h2 className="text-[2rem] font-bold mb-6">
+              Accept and Optimize Payment Globally
+            </h2>
+            <p className="text-[1.1rem] text-stone-600 font-medium mb-6">
+              Keep your business account and all your finance needs safely
+              organized under one roof. Manage money quickly, easily &
+              efficiently. Whether you’re alone or leading a team.
+            </p>
+            <Button>Learn more &rarr;</Button>
+          </div>
+          <div>
+            <img src="feature1.png" alt="Feature" />
+          </div>
+
+          <div>
+            <span className="text-sm text-stone-400">Custom card</span>
+            <h2 className="text-[2rem] font-bold mb-6">
+              Build a Fintech with Banking as a Service
+            </h2>
+            <p className="text-[1.1rem] text-stone-600 font-medium mb-6">
+              Keep your business account and all your finance needs safely
+              organized under one roof. Manage money quickly, easily &
+              efficiently. Whether you’re alone or leading a team.
+            </p>
+            <Button>Learn more &rarr;</Button>
+          </div>
+          <div>
+            <img src="feature2.png" alt="Feature" />
+          </div>
+
+          <div>
+            <span className="text-sm text-stone-400">Built for Growth</span>
+            <h2 className="text-[2rem] font-bold mb-6">
+              Take Your Business Faster
+            </h2>
+            <p className="text-[1.1rem] text-stone-600 font-medium mb-6">
+              Keep your business account and all your finance needs safely
+              organized under one roof. Manage money quickly, easily &
+              efficiently. Whether you’re alone or leading a team.
+            </p>
+            <Button>Learn more &rarr;</Button>
+          </div>
+          <div>
+            <img src="feature2.png" alt="Feature" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Features;
--- frontend/src/components/Overviews.jsx
+++ frontend/src/components/Overviews.jsx
@@ -1,5 +1,47 @@
 function Overviews() {
-  return <div>Overviews</div>;
+  return (
+    <section className="bg-primary-shade">
+      <div className="w-[90%] max-w-[100rem] mx-auto">
+        <div>
+          <div className="py-6">
+            <div>
+              <span className="text-sm text-primary-tint">Global Scale</span>
+              <h2 className="text-[2rem] font-semibold mb-6 text-white">
+                Sell Everywhere In Global Commerce
+              </h2>
+              <p className="text-[1rem] text-white opacity-[0.8] font-medium mb-6">
+                When you sell your software products to global customers,
+                accepting local payment methods is critical in capturing the
+                total market opportunity. Don’t let payment friction get in the
+                way of a single sale. Allow customers to pay in their local
+                currency.
+              </p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="">
+                <span className="text-primary-tint font-bold text-lg">
+                  132+
+                </span>
+                <p className="text-white">Currencies supported</p>
+              </div>
+              <div className="">
+                <span className="text-primary-tint font-bold text-lg">
+                  85.5%
+                </span>
+                <p className="text-white">Business using Finto</p>
+              </div>
+              <div className="">
+                <span className="text-primary-tint font-bold text-lg">
+                  150M
+                </span>
+                <p className="text-white">API request per day</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Overviews;
