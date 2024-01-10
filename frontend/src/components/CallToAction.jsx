0a66a44302427a809b00df4e0b7920f16eb7c958
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-10 21:01:46 +0100

    Update CallToAction.jsx

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -1,5 +1,22 @@
+import Button from "../ui/Button";
+
 function CallToAction() {
-  return <div>CallToAction</div>;
+  return (
+    <section className="bg-primary-shade py-[6rem]">
+      <div className="w-[90%] max-w-[100rem] mx-auto">
+        <h1 className="text-[#fff] font-semibold text-[2.6rem] mb-6 text-center">
+          Simplifying Payments for Growing Business
+        </h1>
+        <p className="text-[#ffffff] text-[1.2rem] opacity-[0.8] mb-8 text-center">
+          Join over 300+ partners and customers already growing with Benefit
+        </p>
+        <div className="grid grid-cols-1 gap-4">
+          <Button type="primary">Open an Account</Button>
+          <Button type="inverted">Contact Sales</Button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default CallToAction;
