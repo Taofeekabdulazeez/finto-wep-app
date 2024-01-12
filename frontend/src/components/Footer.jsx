1dc48aa4468a4f6f6680e1e3091ff0215780c517
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-12 20:51:44 +0100

    m

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,136 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-8">
+      <div className="w-[90%] max-w-[100rem] mx-auto">
+        <div className="mb-6">
+          <img src="footerLogo.png" alt="finto logo" />
+        </div>
+        <nav className="grid grid-cols-2 gap-8 border-b-2 py-6">
+          <ul className="flex flex-col gap-3">
+            <li>Features</li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Payment Link
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Recurring Billing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Invoicing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Checkout
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Integration
+              </a>
+            </li>
+          </ul>
+
+          <ul className="flex flex-col gap-3">
+            <li>Features</li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Payment Link
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Recurring Billing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Invoicing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Checkout
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Integration
+              </a>
+            </li>
+          </ul>
+
+          <ul className="flex flex-col gap-3">
+            <li>Features</li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Payment Link
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Recurring Billing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Invoicing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Checkout
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Integration
+              </a>
+            </li>
+          </ul>
+
+          <ul className="flex flex-col gap-3">
+            <li>Features</li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Payment Link
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Recurring Billing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Invoicing
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Checkout
+              </a>
+            </li>
+            <li>
+              <a className="text-sm font-medium" href="#">
+                Integration
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <p>2023 Finto. All right reserved</p>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
