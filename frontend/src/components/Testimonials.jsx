01fd51958f3ab706ca2fb6d48e9d2048d415826a
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-19 18:23:05 +0100

    Update Testimonials.jsx

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -2,6 +2,12 @@ function Testimonials() {
   return (
     <section className="py-[9rem]">
       <div className="w-[90%] max-w-[100rem] mx-auto">
+        <div className="mb-8">
+          <span className="inline-block text-sm font-semibold uppercase tracking-wide text-primary-tint mb-4">
+            Testimonials
+          </span>
+          <h2 className="text-[2rem] font-semibold mb-6">What customer says</h2>
+        </div>
         <div className="media-scroller mask-gradient">
           <Quote />
           <Quote />
@@ -17,7 +23,7 @@ function Testimonials() {
 
 function Quote() {
   return (
-    <div className="relative p-4 shadow-md rounded-lg before:content-['\201C'] before:text-primary-tint before:text-[3rem] before:absolute before:top-0">
+    <div className="relative p-4 shadow-md rounded-lg before:content-['\201C'] before:text-primary-tint before:text-[4rem] before:absolute before:top-0">
       <p className="mb-8 mt-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
         veniam obcaecati quam, tenetur similique in atque unde sit et cumque
@@ -31,7 +37,7 @@ function Quote() {
             className="block w-[85%] mx-auto absolute bottom-0 translate-x-[10%]"
           />
         </div>
-        <div>
+        <div className="flex flex-col gap-1">
           <p className="text-stone-900 font-medium text-sm">Alex bergwin</p>
           <p className="text-stone-700 text-xs">Account at McDonald</p>
         </div>
