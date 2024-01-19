75703377e185ef6731b9690ca9b5218df64201aa
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-19 19:21:42 +0100

    m

--- frontend/src/components/Operations.jsx
+++ frontend/src/components/Operations.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Slider from "./Slider";
+import Proptypes from "prop-types";
+import { slides } from "../data/data";
 
 function Operations() {
+  const [interchange] = useState(false);
+
   return (
     <section className="py-[8rem]">
       <div className="w-[90%] max-w-[100rem] mx-auto">
@@ -16,10 +21,42 @@ function Operations() {
             lobortis integer purus consectetur. Parturient ut tempor massa mi
           </p>
         </div>
-        <Slider />
+        {interchange && <Slider />}
+
+        <div className="media-scroller-2">
+          {slides.map((slide, index) => (
+            <Operation
+              image={slide.image}
+              description={slide.description}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
 }
+
+function Operation({ image, description }) {
+  return (
+    <div className="rounded overflow-hidden shadow-lg">
+      <div className="bg-slate-200">
+        <img className="w-[100%]" src={image} alt="Artificial Intelligence" />
+      </div>
+      <div className="p-4">
+        <h4 className="font-semibold mb-3 text-lg capitalize">{description}</h4>
+        <p className="text-sm text-stone-700 opacity-[0.8] font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+          doloremque quia aliquam atque
+        </p>
+      </div>
+    </div>
+  );
+}
+
+Operation.propTypes = {
+  image: Proptypes.string,
+  description: Proptypes.string,
+};
 
 export default Operations;
