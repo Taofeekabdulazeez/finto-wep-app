86ac86602ae41f4e4786e93da2bc849dd92ce6d6
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-01-19 20:38:46 +0100

    m

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -1,3 +1,6 @@
+import Proptypes from "prop-types";
+import { testimonials } from "../data/data";
+
 function Testimonials() {
   return (
     <section className="py-[9rem]">
@@ -9,19 +12,25 @@ function Testimonials() {
           <h2 className="text-[2rem] font-semibold mb-6">What customer says</h2>
         </div>
         <div className="media-scroller mask-gradient">
-          <Quote />
-          <Quote />
-          <Quote />
-          <Quote />
-          <Quote />
-          <Quote />
+          {testimonials.map((testimonial) => {
+            const { fullName, image, occupation } = testimonial;
+
+            return (
+              <Quote
+                fullName={fullName}
+                image={image}
+                occupation={occupation}
+                key={fullName}
+              />
+            );
+          })}
         </div>
       </div>
     </section>
   );
 }
 
-function Quote() {
+function Quote({ fullName, image, occupation }) {
   return (
     <div className="relative p-4 shadow-md rounded-lg before:content-['\201C'] before:text-primary-tint before:text-[4rem] before:absolute before:top-0">
       <p className="mb-8 mt-8">
@@ -32,18 +41,24 @@ function Quote() {
       <div className="flex items-center gap-6">
         <div className="h-9 w-9 rounded-full bg-primary-50 overflow-hidden relative">
           <img
-            src="customer-1.png"
+            src={image}
             alt="customer img"
             className="block w-[85%] mx-auto absolute bottom-0 translate-x-[10%]"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <p className="text-stone-900 font-medium text-sm">Alex bergwin</p>
-          <p className="text-stone-700 text-xs">Account at McDonald</p>
+          <p className="text-stone-900 font-medium text-sm">{fullName}</p>
+          <p className="text-stone-700 text-xs">{occupation}</p>
         </div>
       </div>
     </div>
   );
 }
+
+Quote.propTypes = {
+  fullName: Proptypes.string,
+  image: Proptypes.string,
+  occupation: Proptypes.string,
+};
 
 export default Testimonials;
--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -3,3 +3,31 @@ export const slides = [
   { image: "operation2.png", description: "validate your idea" },
   { image: "operation3.png", description: "Sell to customers" },
 ];
+
+export const testimonials = [
+  {
+    image: "customer-1.png",
+    fullName: "Sarah Johnson",
+    occupation: "Marketing Consultant",
+  },
+  {
+    image: "customer-2.png",
+    fullName: "Micheal Rodriguez",
+    occupation: "Software Engineer",
+  },
+  {
+    image: "customer-3.png",
+    fullName: "Emily Lara",
+    occupation: "Finacial Analyst",
+  },
+  {
+    image: "customer-4.png",
+    fullName: "Lisa Patel",
+    occupation: "Registered Nurse",
+  },
+  {
+    image: "customer-5.png",
+    fullName: "Rajesh Kumar",
+    occupation: "Architect",
+  },
+];
